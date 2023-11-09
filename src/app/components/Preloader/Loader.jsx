ec7095a6e5d5786f0af37740768cb043fc08256a
ccadran <clariocadran33@gmail.com>
2023-11-09 15:09:47 +0100

    start loader anim

--- src/app/components/Preloader/Loader.jsx
+++ src/app/components/Preloader/Loader.jsx
@@ -3,7 +3,8 @@
 import styles from "./style.module.scss";
 import Image from "next/image";
 import { imageDown } from "./anim";
-import motion from "framer-motion";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 
 const images = [
   "/assets/images/blobism.jpg",
@@ -14,21 +15,51 @@ const images = [
 ];
 
 export default function Loader() {
+  const imageRefs = useRef([]);
+
+  useLayoutEffect(() => {
+    const timeline = gsap.timeline({ defaults: { delay: 0 } });
+
+    timeline.to(
+      imageRefs.current[0],
+      { top: "50%", transform: "translateY(-50%)" },
+      0.2
+    );
+    timeline.to(
+      imageRefs.current[1],
+      { top: "50%", transform: "translateY(-50%)" },
+      0.5
+    );
+    timeline.to(
+      imageRefs.current[2],
+      { top: "50%", transform: "translateY(-50%)" },
+      0.75
+    );
+    timeline.to(
+      imageRefs.current[3],
+      { top: "50%", transform: "translateY(-50%)" },
+      0.85
+    );
+    timeline.to(
+      imageRefs.current[4],
+      { top: "50%", transform: "translateY(-50%)" },
+      0.9
+    );
+  });
+
   return (
     <div className={styles.loader}>
       <h1>Architecture</h1>
       <div className={styles.imageContainer}>
         {images.map((image, index) => (
-          <motion.Image
+          <Image
             src={image}
             className={styles.image}
             width={200}
             height={300}
             objectFit="cover"
             key={index}
-            variants={imageDown}
-            initial="initial"
-            animate="open"
+            ref={(el) => (imageRefs.current[index] = el)}
           />
         ))}
       </div>
