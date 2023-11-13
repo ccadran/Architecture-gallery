42b43df60cc5728d0dcdd4e507d6d07ac5a723da
ccadran <clariocadran33@gmail.com>
2023-11-13 15:09:40 +0100

    remove imports from page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,7 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Nav from "./components/Nav/Nav";
-import Controller from "./components/SliderController/Controller";
-import Content from "./components/MainContent/Content";
 import content from "../content/content.json";
-import NextImageSlider from "./components/NextImageSlider/NextImageSlider";
 import HeroSlider from "./components/HeroSlider/Hero";
 import gsap from "gsap";
 import { useState } from "react";
