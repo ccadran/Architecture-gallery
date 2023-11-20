16a140aec7e29ee3a9d6e09ef7975e7549c26432
ccadran <clariocadran33@gmail.com>
2023-11-20 12:18:06 +0100

    add animation of the img scale on scroll

--- src/app/components/Content/Card/Card.jsx
+++ src/app/components/Content/Card/Card.jsx
@@ -1,12 +1,26 @@
+import { useRef } from "react";
 import styles from "./card.module.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+
 export default function Card({ src, title, shortDescription, color }) {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+  const scale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+
   return (
-    <div className={styles.cardContainer}>
+    <div ref={container} className={styles.cardContainer}>
       <div style={{ backgroundColor: color }} className={styles.card}>
         <h2>{title}</h2>
         <p>{shortDescription}</p>
         <div className={styles.imgContainer}>
-          <img src={src} alt="" />
+          <motion.div style={{ scale }} className={styles.inner}>
+            <img src={src} alt="image" />
+          </motion.div>
         </div>
       </div>
     </div>
