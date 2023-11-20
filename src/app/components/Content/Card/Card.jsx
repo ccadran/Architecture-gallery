d2f548e395abfbe9951dfcfcd98c9c24d7cb69c2
ccadran <clariocadran33@gmail.com>
2023-11-20 11:39:04 +0100

    add sticky position to cards

--- src/app/components/Content/Card/Card.jsx
+++ src/app/components/Content/Card/Card.jsx
@@ -2,7 +2,10 @@ import styles from "./card.module.scss";
 export default function Card({ src, title, shortDescription, color }) {
   return (
     <div className={styles.cardContainer}>
-      <div style={{ backgroundColor: color }} className={styles.card}></div>
+      <div style={{ backgroundColor: color }} className={styles.card}>
+        <h2>{title}</h2>
+        <p>{shortDescription}</p>
+      </div>
     </div>
   );
 }
