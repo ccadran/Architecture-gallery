3be74531e4791cb42d291ce0493e9f0ebedd81bf
ccadran <clariocadran33@gmail.com>
2023-11-20 19:51:50 +0100

    add reveal of the footer on scroll

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,13 +1,25 @@
+import { useRef } from "react";
 import styles from "./footer.module.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Footer() {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start center", "end end"],
+  });
+
+  const height = useTransform(scrollYProgress, [0, 1], ["100vh", "0vh"]);
   return (
-    <div className={styles.footerContainer}>
+    <div ref={container} className={styles.footerContainer}>
       <div className={styles.footer}>
         <h1>Footer</h1>
       </div>
-      <div className={styles.footerTransition}></div>
+      <motion.div
+        style={{ height }}
+        className={styles.footerTransition}
+      ></motion.div>
     </div>
   );
 }
