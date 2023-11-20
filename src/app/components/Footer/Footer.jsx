5697f07dc8cfa3c11ade5d0d90422c7761270081
ccadran <clariocadran33@gmail.com>
2023-11-20 21:17:56 +0100

    start footer

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import styles from "./footer.module.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
+import content from "../../../content/content.json";
 
 export default function Footer() {
   const container = useRef(null);
@@ -10,16 +11,49 @@ export default function Footer() {
     offset: ["start center", "end end"],
   });
 
-  const height = useTransform(scrollYProgress, [0, 1], ["100vh", "0vh"]);
+  const numColumns = 10;
+
+  // Utilisez la propriété 'map' pour créer une valeur spécifique pour chaque colonne
+  const heights = [...Array(numColumns)].map((_, index) =>
+    useTransform(scrollYProgress, [0, 1], [`${100 + index * 25}vh`, "0vh"])
+  );
+
   return (
     <div ref={container} className={styles.footerContainer}>
       <div className={styles.footer}>
-        <h1>Footer</h1>
+        <div className={styles.credits}>
+          {content.map((item, index) => {
+            return (
+              <div className={styles.credit}>
+                <div className={styles.imgContainer}>
+                  <img src={item.src} alt="" />
+                </div>
+                <p>{item.author}, via Unsplash</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className={styles.presentation}>
+          <img src="" alt="qr code" />
+          <h2>Designed and developed by Clario Cadran</h2>
+          <div className={styles.socials}>
+            <a href="https://github.com/ccadran">Linkedin</a>
+            <a href="https://github.com/ccadran">GitHub</a>
+            <a href="https://github.com/ccadran">Instagram</a>
+          </div>
+        </div>
       </div>
-      <motion.div
-        style={{ height }}
-        className={styles.footerTransition}
-      ></motion.div>
+      <motion.div className={styles.footerColumnContainer}>
+        {heights.map((height, index) => (
+          <FooterColumn key={index} height={height} />
+        ))}
+      </motion.div>
     </div>
+  );
+}
+
+export function FooterColumn({ height }) {
+  return (
+    <motion.div style={{ height }} className={styles.footerColumn}></motion.div>
   );
 }
