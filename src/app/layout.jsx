2a34a2001e5f1368f2d464931f50c7c088363bc5
ccadran <clariocadran33@gmail.com>
2023-11-20 18:35:05 +0100

    correction ont the loader z-index

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,12 +6,12 @@ import Preloader from "./components/Preloader/Loader";
 import { AnimatePresence } from "framer-motion";
 
 export default function RootLayout({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // setTimeout(() => {
-    //   setIsLoading(false);
-    // }, 3250);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3250);
   });
   return (
     <html lang="en">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,12 +14,10 @@ import Lenis from "@studio-freight/lenis";
 export default function Home() {
   useEffect(() => {
     const lenis = new Lenis();
-
     function raf(time) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
   }, []);
   return (
