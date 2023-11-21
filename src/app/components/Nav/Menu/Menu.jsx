500087e645e96bf22f9aebaa21f2e2c089bbd9ad
ccadran <clariocadran33@gmail.com>
2023-11-21 20:47:10 +0100

    designed menu w/o anim

--- src/app/components/Nav/Menu/Menu.jsx
+++ src/app/components/Nav/Menu/Menu.jsx
@@ -1,4 +1,23 @@
 import styles from "./menu.module.scss";
+import content from "../../../../content/content.json";
+
 export default function Menu() {
-  return <div className={styles.menu}>Menu</div>;
+  console.log(content.map((item) => item.genre));
+  return (
+    <div className={styles.menu}>
+      <div className={styles.genres}>
+        {content.map((item, index) => {
+          return (
+            <div className={styles.genre}>
+              <div className={styles.text}>
+                <p>( {index + 1} )</p>
+                <h2>{item.genre}</h2>
+              </div>
+              <div className={styles.line}></div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
