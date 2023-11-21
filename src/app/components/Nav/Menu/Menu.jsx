0fe6b69d7dc1c35746f5c04dc05da5b88b2c5cc0
ccadran <clariocadran33@gmail.com>
2023-11-21 20:18:04 +0100

    add a menu

---
+++ src/app/components/Nav/Menu/Menu.jsx
@@ -0,0 +1,4 @@
+import styles from "./menu.module.scss";
+export default function Menu() {
+  return <div className={styles.menu}>Menu</div>;
+}
--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import styles from "./nav.module.scss";
+import Menu from "./Menu/Menu";
 
 export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log("open");
+  };
   return (
-    <div className={styles.nav}>
-      <h4 className={styles.menu}>( Menu )</h4>
+    <div className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
+      <h4 className={styles.menuOpener} onClick={() => toggleMenu()}>
+        ( Menu )
+      </h4>
       <div className={styles.credit}>
         <div className={styles.copyright}>@</div>
         <div className={styles.name}>
@@ -12,6 +22,12 @@ export default function Nav() {
           <p className={styles.cadran}>Cadran</p>
         </div>
       </div>
+      {isMenuOpen && (
+        <div className={styles.close} onClick={() => setIsMenuOpen(false)}>
+          <h4>close</h4>
+        </div>
+      )}
+      {isMenuOpen && <Menu />}
     </div>
   );
 }
