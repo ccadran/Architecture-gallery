dcb0652747adaf01c5b7b08185ccb93ca73baa30
ccadran <clariocadran33@gmail.com>
2023-12-04 09:50:34 +0100

    add pointer on hover menu

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -16,14 +16,14 @@ export default function Nav() {
       <h4 className={styles.menuOpener} onClick={() => toggleMenu()}>
         ( Menu )
       </h4>
-      <div className={styles.credit}>
+      {/* <div className={styles.credit}>
         <div className={styles.copyright}>@</div>
         <div className={styles.name}>
           <p className={styles.codeBy}>Code By</p>
           <p className={styles.clario}>Clario</p>
           <p className={styles.cadran}>Cadran</p>
         </div>
-      </div>
+      </div> */}
       <AnimatePresence mode="wait">
         {isMenuOpen && (
           <motion.div
